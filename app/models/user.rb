81b96c8df59a246ebe067edee17136745e2074d2
s-i27 <s_d27@icloud.com>
2020-10-20 11:10:59 +0900

    ロボコップでのコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # 大文字小文字を区別したい場合は{ case_sensitive: true }を仕様（DB側は区別しない）
-  validates :nickname,         presence: true,uniqueness:  { case_sensitive: true }
-  validates :last_name,        presence: true, format: { with: /\A[一-龥ぁ-ん]/, message: 'Full-width characters'}
-  validates :first_name,       presence: true, format: { with: /\A[一-龥ぁ-ん]/, message: 'Full-width characters'}
-  validates :last_name_kana,   presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width katakana characters'}
-  validates :first_name_kana,  presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width katakana characters'}
+  validates :nickname,         presence: true, uniqueness: { case_sensitive: true }
+  validates :last_name,        presence: true, format: { with: /\A[一-龥ぁ-ん]/, message: 'Full-width characters' }
+  validates :first_name,       presence: true, format: { with: /\A[一-龥ぁ-ん]/, message: 'Full-width characters' }
+  validates :last_name_kana,   presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width katakana characters' }
+  validates :first_name_kana,  presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'Full-width katakana characters' }
   validates :birthday,         presence: true
-  validates :password,         format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers'}
+  validates :password,         format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers' }
 end
