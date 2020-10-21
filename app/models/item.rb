d66b5b392df7b56cd55a8310bab02b2de62f167e
s-i27 <s_d27@icloud.com>
2020-10-22 00:26:36 +0900

    Rubocop実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,20 +9,20 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_date
 
   with_options presence: true do
-    validates :image 
+    validates :image
     validates :name
     validates :comment
     validates :price
     validates :category
     validates :status
     validates :shipping_at
-    validates :shipping_area 
+    validates :shipping_area
     validates :shipping_date
   end
 
   validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than: 9_999_999, message: 'Out of setting range' }
-  
+
   # ジャンルの選択が「--」の時は保存できないようにする
   with_options numericality: { other_than: 1, message: 'Select' } do
     validates :category_id
