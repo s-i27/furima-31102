07c072239cc9bfa4b98f43be2c5135fb24007d96
s-i27 <s_d27@icloud.com>
2020-10-21 22:11:26 +0900

    バリデーションの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -72,7 +72,7 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      it 'ユーザーが紐付いていないとツイートは保存できない' do
+      it 'ユーザーが紐付いていないとitemは保存できない' do
         @item.user = nil
         @item.valid?
         expect(@item.errors.full_messages).to include('User must exist')
