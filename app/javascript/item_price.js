4ad47b38f3222711bedac938d7e2d4ef55174c70
s-i27 <s_d27@icloud.com>
2020-10-21 19:16:33 +0900

    Javascriptファイル作成（コンソールログへの出力確認）

---
+++ app/javascript/item_price.js
@@ -0,0 +1,12 @@
+function post (){
+  const priceInput = document.getElementById("item-price");
+  const taxPrice = document.getElementById("add-tax-price");
+  const profitInput = document.getElementById("profit");
+
+
+
+  console.log(taxPrice);
+
+}
+
+window.addEventListener('load', post);
