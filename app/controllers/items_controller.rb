9f246591658a5ffef9a4b593668dda75bee307f7
s-i27 <s_d27@icloud.com>
2020-10-21 14:52:35 +0900

    クリエイトアクション内を記載

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,16 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
   def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+ private
+  def item_params
+    params.require(:item).permit(:image,:name,:comment,:category_id,:shipping_area_id,:shipping_at_id,:shipping_date_id,:status_id,:price).merge(user_id: current_user.id)
+  end  
 end
