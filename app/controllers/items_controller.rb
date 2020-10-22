ccfad0466881998e5ecbd26e1de7aca860ecfcdf
s-i27 <s_d27@icloud.com>
2020-10-22 13:23:04 +0900

    INDEXへのリダイレクト追加、ロボコップ実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit] 
-  
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: [:edit]
+
   def index
     @items = Item.all
   end
@@ -29,13 +30,16 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-        redirect_to item_path
+      redirect_to item_path
     else
-        render :edit
-    end 
+      render :edit
+    end
+  end
 
-  end 
-
+  def move_to_index
+    item = Item.find(params[:id])
+    redirect_to action: :index if current_user.id != item.user_id
+  end
 
   private
 
