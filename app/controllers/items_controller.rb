6bf4eb24dfeb5c1b32eeabfb913f1db3559d45fe
s-i27 <s_d27@icloud.com>
2020-10-22 14:20:32 +0900

    @item = Item.find(params[:id])の記述を省略

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, only: [:edit]
+  before_action :item_find, only: [:show, :edit, :update]
 
   def index
     @items = Item.all
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -36,14 +34,20 @@ class ItemsController < ApplicationController
     end
   end
 
-  def move_to_index
-    item = Item.find(params[:id])
-    redirect_to action: :index if current_user.id != item.user_id
-  end
-
   private
+    def item_params
+      params.require(:item).permit(:image, :name, :comment, :category_id, :shipping_area_id, :shipping_at_id, :shipping_date_id, :status_id, :price).merge(user_id: current_user.id)
+    end
 
-  def item_params
-    params.require(:item).permit(:image, :name, :comment, :category_id, :shipping_area_id, :shipping_at_id, :shipping_date_id, :status_id, :price).merge(user_id: current_user.id)
-  end
+    def move_to_index
+      item = Item.find(params[:id])
+      if current_user.id != item.user_id
+        redirect_to action: :index 
+      end
+    end
+  
+    def item_find
+      @item = Item.find(params[:id])
+    end
+    
 end
