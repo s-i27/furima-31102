f05c836f25015573ae554ba7207fb3aa373057b2
s-i27 <s_d27@icloud.com>
2020-10-22 14:50:45 +0900

    destroアクション、ビューファイル追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,6 +28,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end 
+
   private
     def item_params
       params.require(:item).permit(:image, :name, :comment, :category_id, :shipping_area_id, :shipping_at_id, :shipping_date_id, :status_id, :price).merge(user_id: current_user.id)
