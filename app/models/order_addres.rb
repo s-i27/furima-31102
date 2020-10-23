88708055e385a609458d23f53e35f03c84e36d46
s-i27 <s_d27@icloud.com>
2020-10-23 18:26:46 +0900

    バリデーションの記載

--- app/models/order_addres.rb
+++ app/models/order_addres.rb
@@ -1,21 +1,27 @@
 class OrderAddres
   include ActiveModel::Model
   attr_accessor :postcode, :prefecture_id, :building, :block, :city, :phone_number,:user_id, :item_id
-with_options presence: true do
+
+  with_options presence: true do
   validates :postcode
   validates :prefecture_id
   validates :block
   validates :city
   validates :phone_number
-end
+  end
 
-  # ジャンルの選択が「--」の時は保存できないようにする
+    # ジャンルの選択が「--」の時は保存できないようにする
   validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
+    # 正規表現の指定
+  validates :postcode,     format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
+  validates :phone_number, format: { with: /\A\d{11}\z/, message: 'Input only number' }
+
+
 
   def save
     # 購入情報の情報を保存
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postcode: postcode, prefecture_id: prefecture_id, building: building,block: block, city: city, phone_number: phone_number, order_id:order.id)
     # 住所の情報を保存
+    Address.create(postcode: postcode, prefecture_id: prefecture_id, building: building,block: block, city: city, phone_number: phone_number, order_id:order.id)
   end
 end
