ea4422a6877c5c55261d12ab89518ed2c0a8b516
s-i27 <s_d27@icloud.com>
2020-10-23 18:08:56 +0900

    アクション記載、INDEXビューファイル修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,25 @@
 class OrdersController < ApplicationController
   def index
-    @order_addres = Order_Addres.new
+    @item = Item.find(params[:item_id])
+    @order_addres = OrderAddres.new
   end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_addres = OrderAddres.new(order_address_params)
+    
+    if @order_addres.valid?
+        @order_addres.save
+        redirect_to action: :index
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def order_address_params
+    params.require(:order_addres).permit(:postcode, :prefecture_id, :building, :block, :city, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
 end
--- app/models/order_addres.rb
+++ app/models/order_addres.rb
@@ -1,7 +1,6 @@
 class OrderAddres
-  include ActiveModel: Model:
-  attr_accessor :postal_code, :prefecture, :building, :block, :city, :phone_number
-
+  include ActiveModel::Model
+  attr_accessor :postcode, :prefecture_id, :building, :block, :city, :phone_number,:user_id, :item_id
 with_options presence: true do
   validates :postcode
   validates :prefecture_id
@@ -14,12 +13,9 @@ end
   validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
 
   def save
-    # ユーザーの情報を保存し、「user」という変数に入れている
-    user = User.all
-    item = Item.all
     # 購入情報の情報を保存
-    Donation.create(user_id: user.id, item_id: item.id)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postcode: postcode, prefecture_id: prefecture_id, building: building,block: block, city: city, phone_number: phone_number, order_id:order.id)
     # 住所の情報を保存
-    Address.create(postalcode: postalcode, prefecture: prefecture, building: building,block: block, city: city, phone_number: phone_number,buy_id: buy.id)
   end
 end
