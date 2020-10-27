8151ee9886164e4eedd2701babe489b83e12380a
s-i27 <s_d27@icloud.com>
2020-10-28 08:27:33 +0900

    クライアントサイド実装

---
+++ app/javascript/card.js
@@ -0,0 +1,34 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_f030c56c47af2a3ea47acfe5"); // PAY.JPテスト公開鍵
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order_addres[number]"),
+      exp_month: formData.get("order_addres[month]"),
+      exp_year: `20${formData.get("order_addres[year]")}`,
+      cvc: formData.get("order_addres[code]"),
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+      document.getElementById("card_number").removeAttribute("name");
+      document.getElementById("card_cvc").removeAttribute("name");
+      document.getElementById("card_exp_month").removeAttribute("name");
+      document.getElementById("card_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+    });
+  });
+};
+
+window.addEventListener("load", pay);
